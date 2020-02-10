5f638fc41b0516e565a2a89fd766d2c3f497ca32
Ramola K <ramolagk14@gmail.com>
2020-02-10 14:02:38 +0530

    update

--- src/main/java/com/bookshelf/implementations/LoginDAOImpl.java
+++ src/main/java/com/bookshelf/implementations/LoginDAOImpl.java
@@ -12,9 +12,9 @@ public class LoginDAOImpl implements LoginDAO{
 
 	public void newLogin(Login ab) throws Exception {
 		String query="insert into login(login_no,user_name,email,phone_no,preference_type,password)values (login_sqc.nextval,?,?,?,?,?)";
-		Connection con=DbConnection.getConnection();
 		
-		PreparedStatement pst = con.prepareStatement(query);
+		try(Connection con=DbConnection.getConnection();PreparedStatement pst = con.prepareStatement(query))
+		{
 		pst.setString(1,ab.userName);
 		pst.setString(2,ab.eMail);
 		pst.setLong(3,ab.phoneNo);
@@ -24,14 +24,19 @@ public class LoginDAOImpl implements LoginDAO{
 		int rows=pst.executeUpdate();
 		System.out.println("No of rows inserted :"+rows);
 		con.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 
 	public String userLoginValidation(String eMail,String password) throws Exception {
 		// TODO Auto-generated method stub
 		String query="select email,password from login where email=? and password=?";
-		Connection con=DbConnection.getConnection();
 		
-		PreparedStatement pst = con.prepareStatement(query);
+		try(Connection con=DbConnection.getConnection();PreparedStatement pst = con.prepareStatement(query))
+		{
 		
 		pst.setString(1,eMail);
 		pst.setString(2,password);
@@ -49,9 +54,14 @@ public class LoginDAOImpl implements LoginDAO{
 			s="failure";
 		}
 		con.close();
+		
 		return(s);
-
-
+		}
+		catch(Exception e)
+		{
+			
+		}
+		return null;
 		
 	}
 
